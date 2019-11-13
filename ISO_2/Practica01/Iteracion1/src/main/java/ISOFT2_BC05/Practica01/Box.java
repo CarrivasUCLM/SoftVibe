fd421cd969adf9fa655eaa958439b38352b89dab
4Paloms <32933725+4Paloms@users.noreply.github.com>
2019-11-13 17:45:44 +0100

    Clase Box Actualizada

--- ISO_2/Practica01/Iteracion1/src/main/java/ISOFT2_BC05/Practica01/Box.java
+++ ISO_2/Practica01/Iteracion1/src/main/java/ISOFT2_BC05/Practica01/Box.java
@@ -1,2 +1,46 @@
 
+package ISOFT2_BC05.Practica01;
 
+public class Box {
+
+	private int State;
+	private int Number;
+
+	/**
+	 * 
+	 * @param State
+	 * @param Number
+	 */
+	public Box(int State, int Number) {
+		// TODO - implement Box.Box
+		throw new UnsupportedOperationException();
+	}
+
+	public int getState() {
+		// TODO - implement Box.getState
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param State
+	 */
+	public void setState(int State) {
+		// TODO - implement Box.setState
+		throw new UnsupportedOperationException();
+	}
+
+	public int getNumber() {
+		// TODO - implement Box.getNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Number
+	 */
+	public void setNumber(int Number) {
+		// TODO - implement Box.setNumber
+		throw new UnsupportedOperationException();
+	}
+}
