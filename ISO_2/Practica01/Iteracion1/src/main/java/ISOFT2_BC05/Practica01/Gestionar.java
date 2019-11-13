30ebc02d79d3b298e363b27923551e09fbe8c50b
4Paloms <32933725+4Paloms@users.noreply.github.com>
2019-11-13 17:44:10 +0100

    Iteracion 1 Realizada

---
+++ ISO_2/Practica01/Iteracion1/src/main/java/ISOFT2_BC05/Practica01/Gestionar.java
@@ -0,0 +1,29 @@
+package ISOFT2_BC05.Practica01;
+
+public class Gestionar {
+
+	private Box box;
+
+	/**
+	 * 
+	 * @param box
+	 */
+	public Gestionar(Box box) {
+		// TODO - implement Gestionar.Gestionar
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param box
+	 */
+	public void Eliminar(Box box) {
+		// TODO - implement Gestionar.Eliminar
+		throw new UnsupportedOperationException();
+	}
+
+	public Box Añadir() {
+		// TODO - implement Gestionar.Añadir
+		throw new UnsupportedOperationException();
+	}
+}
