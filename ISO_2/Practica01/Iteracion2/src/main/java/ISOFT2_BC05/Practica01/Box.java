5fa4a8c523b9a0863eedaf200d860fdad12ffd12
Carlos <Carlos@Rivas>
2019-11-18 20:34:21 +0100

    Rama 3 actualizada

---
+++ ISO_2/Practica01/Iteracion2/src/main/java/ISOFT2_BC05/Practica01/Box.java
@@ -0,0 +1,40 @@
+public class Box {
+
+	private int number;
+	private int state;
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	/**
+	 * 
+	 * @param Number
+	 */
+	public void setNumber(int Number) {
+		this.number = Number;
+	}
+
+	public int getState() {
+		return this.state;
+	}
+
+	/**
+	 * 
+	 * @param State
+	 */
+	public void setState(int State) {
+		this.state = State;
+	}
+
+	/**
+	 * 
+	 * @param number
+	 * @param state
+	 */
+	public Box(int number, int state) {
+		// TODO - implement Box.Box
+		throw new UnsupportedOperationException();
+	}
+
+}
