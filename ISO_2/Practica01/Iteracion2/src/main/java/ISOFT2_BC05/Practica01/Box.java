9ab5dfafced310e2ec33a5886598132b04d6117c
Carlos <Carlos@Rivas>
2019-11-18 20:28:31 +0100

    Clase GestionarBox actualizada

---
+++ ISO_2/Practica01/Iteracion2/src/main/java/ISOFT2_BC05/Practica01/Box.java
@@ -0,0 +1,28 @@
+public class Box {
+
+	private int number;
+	private int state;
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(int Number) {
+		this.number = Number;
+	}
+
+	public int getState() {
+		return this.state;
+	}
+
+
+	public void setState(int State) {
+		this.state = State;
+	}
+
+	public Box(int number, int state) {
+		
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ ISO_2/Practica01/Iteracion2/src/main/java/ISOFT2_BC05/Practica01/GestionarBox.java
@@ -0,0 +1,35 @@
+public class GestionarBox {
+
+	private Box box;
+
+	/**
+	 * 
+	 * @param box
+	 */
+	public GestionarBox(Box box) {
+		// TODO - implement GestionarBox.GestionarBox
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Box
+	 */
+	public void EliminarBox(Box Box) {
+		// TODO - implement GestionarBox.EliminarBox
+		throw new UnsupportedOperationException();
+	}
+
+	public void CambiarEstado() {
+		// TODO - implement GestionarBox.CambiarEstado
+		throw new UnsupportedOperationException();
+	}
+
+	public void SeleccionaBox() {
+		System.out.println("Selecciona el box a utilizar: ");
+		
+		System.out.println("Box seleccionado");
+		throw new UnsupportedOperationException();
+	}
+
+}
