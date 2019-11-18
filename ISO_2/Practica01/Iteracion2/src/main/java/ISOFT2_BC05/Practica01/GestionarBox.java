f2afa9f5286c3df6c11bf8e3a97691aa139b862e
Carlos <Carlos@Rivas>
2019-11-18 20:12:25 +0100

    Clase GestionarBox actualizada

---
+++ ISO_2/Practica01/Iteracion2/src/main/java/ISOFT2_BC05/Practica01/GestionarBox.java
@@ -0,0 +1,33 @@
+public class GestionarBox {
+
+	private Box box;
+
+	/**
+	 * 
+	 * @param box
+	 */
+	public GestionarBox(Box box) {
+		// TODO - implement GestionarBox.GestionarBox
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Box
+	 */
+	public void EliminarBox(Box Box) {
+		// TODO - implement GestionarBox.EliminarBox
+		throw new UnsupportedOperationException();
+	}
+
+	public void CambiarEstado() {
+		// TODO - implement GestionarBox.CambiarEstado
+		throw new UnsupportedOperationException();
+	}
+
+	public void SeleccionaBox() {
+		// TODO - implement GestionarBox.SeleccionaBox
+		throw new UnsupportedOperationException();
+	}
+
+}
