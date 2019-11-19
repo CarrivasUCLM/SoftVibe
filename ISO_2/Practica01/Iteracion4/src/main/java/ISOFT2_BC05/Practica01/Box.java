4907565145c15fcd730005fb4e26fa0d37251ede
MarioCoboGC <55800707+MarioCoboGC@users.noreply.github.com>
2019-11-19 16:58:13 +0100

    Add files via upload
    Se introduce las clases necesarias para la realización del requisito 4

---
+++ ISO_2/Practica01/Iteracion4/src/main/java/ISOFT2_BC05/Practica01/Box.java
@@ -0,0 +1,17 @@
+public class Box {
+
+	public string ObtenerEstadoBox() {
+		// TODO - implement Box.ObtenerEstadoBox
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param NumeroBox
+	 */
+	public void SeleccionarBox(int NumeroBox) {
+		// TODO - implement Box.SeleccionarBox
+		throw new UnsupportedOperationException();
+	}
+
+}
